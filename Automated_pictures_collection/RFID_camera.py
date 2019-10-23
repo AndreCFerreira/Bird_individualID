cce5243d3ea8e9e9cc0eb8e6ade56463ff5bab1f
AndreCFerreira <52666162+AndreCFerreira@users.noreply.github.com>
2019-10-23 14:14:34 +0100

    Update RFID_camera.py

--- Automated_pictures_collection/RFID_camera.py
+++ Automated_pictures_collection/RFID_camera.py
@@ -4,16 +4,16 @@ import time
 import random
 import datetime
 
-#define the camera settings:
+#define camera settings:
 camera = PiCamera(resolution=(1920,1080))
 camera.shutter_speed = 3000
 camera.iso = 1600
 time.sleep(1)
-#you can consult https://picamera.readthedocs.io/en/release-1.13/index.html for more
+#consult https://picamera.readthedocs.io/en/release-1.13/index.html for more
 #info on camera settings
 
-#iniciate the serial communication check the RFID logger datasheet
-#to get the baudrate and the other settings
+#initialize the serial communication (check the RFID logger datasheet
+#to get the baudrate and the other settings)
 ser = serial.Serial(
   
    port='/dev/ttyUSB0',#ls /dev/tty*
@@ -27,12 +27,13 @@ ser = serial.Serial(
 #in the case of the priorityOne logger the tags are seperated by "\r"
 #for example
 #b'011016BA90\r011016BA90\r011016BA90\r"
-#you can check how data is read by the raspberry pi with the loop
+#to check how data is read by the raspberry pi uncomment the following lines
 #while 1:
 #    x=.readline()
 #    print(x)
-#In the case of priorityOne RIFDlogger we can use this function from:
-#https://stackoverflow.com/questions/45215838/python-serial-pyserial-reading-lines-with-eol-r-instead-of-n?noredirect=1&lq=1
+
+#Read the data in the case of priorityOne RIFDlogger using the following (adapted from:
+#https://stackoverflow.com/questions/45215838/python-serial-pyserial-reading-lines-with-eol-r-instead-of-n?noredirect=1&lq=1)
 def readData():
     buffer = ""
     while True:
@@ -42,12 +43,12 @@ def readData():
         else:
             buffer += oneByte.decode("ascii")
 
-#loop to read from the RFIDlogger
+#Create loop to continuously read from the RFIDlogger
 while 1:
     x=readData()#read the data from the logger
     #print(x)
     if len(x)>9:#if the string has more than 9 characters (our tags have 10 characters)
-                #take a picture. Setting up this  threshold is useful to avoid noise from
+                #take a picture. Setting up this threshold is useful to avoid adding noise from
                 #the serial communication
         #define the name by pasting the tag number with the date and time
         name=str(x)+"_"+str(datetime.datetime.now())[0:10]+"_"+str(datetime.datetime.now())[11:19]
@@ -60,6 +61,7 @@ while 1:
         #in this case we change the shutter_speed
         camera.shutter_speed = random.randint(8000,10000)
         #setup a time limit to take the next picture (to avoid identical pictures)
+        #in this case we defined a time interval of 2 sec
         time.sleep(2)
         #clear the data arriving from the RFIDlogger (as the logger reads the
         #tag every 1/3 of a second)
